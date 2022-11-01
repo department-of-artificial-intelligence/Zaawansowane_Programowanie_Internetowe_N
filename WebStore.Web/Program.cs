c4d0aca3d003ea44037d15a1a394424a7aeab07b
Marta Rak <marta.rak@shc.digital>
2022-11-01 17:24:20 +0100

    Lab 1 problem z migracją

--- WebStore.Web/Program.cs
+++ WebStore.Web/Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext(options => options.UseSqlServer(connectionString));
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
