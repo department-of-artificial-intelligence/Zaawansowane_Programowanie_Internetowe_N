f9ca903aec8fad4e737e3e8cd2e6a0dc693daed4
Marta Rak <marta.rak@shc.digital>
2022-11-06 12:35:12 +0100

    testy pozytywne, zadania lab1 wykonane

---
+++ WebStore.Services/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using WebStore.Model.DataModels;
+using WebStore.ViewModels.VM;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IProductService
+    {
+        public ProductVm AddOrUpdateProduct(AddOrUpdateProductVm addOrUpdateProductVm);
+        public ProductVm GetProduct(Expression<Func<Product, bool>> filterExpression);
+        public IEnumerable<ProductVm> GetProducts(Expression<Func<Product, bool>>? filterExpression = null);
+    }
+}
