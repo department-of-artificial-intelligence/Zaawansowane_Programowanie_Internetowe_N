cf69d9bb903b4abbc6350ce8ba608afc843fe5ce
chyr <chyr@protonmail.com>
2023-10-23 22:24:45 +0200

    fixed addresses

--- WebStore.Model/Customer.cs
+++ WebStore.Model/Customer.cs
@@ -3,6 +3,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace WebStore.Model;
 public class Customer : User
 {
-    public virtual IList<Address> Addresses {get; set;} = new List<Address>();
+    //public virtual IList<Address> Addresses {get; set;} = new List<Address>();
+    public virtual Address? BillingAddress {get; set;}
+    public virtual Address? ShippingAddress {get; set;}
     public virtual IList<Order> Orders {get; set;} =  new List<Order>();    
 }
