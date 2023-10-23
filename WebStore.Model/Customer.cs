c7eef9c21d30ef59dae62f7d04a80afe0ec2b3e0
chyr <chyr@protonmail.com>
2023-10-23 22:17:01 +0200

    removed id from customer

--- WebStore.Model/Customer.cs
+++ WebStore.Model/Customer.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace WebStore.Model;
 public class Customer : User
 {
-    public int Id {get; set;}
     public virtual IList<Address> Addresses {get; set;} = new List<Address>();
     public virtual IList<Order> Orders {get; set;} =  new List<Order>();    
 }
