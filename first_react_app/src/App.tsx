7c3e5fc6d364543441568b990a813d5088c099f2
artur_gonera <fajnymail@example.com>
2023-12-12 20:26:16 +0100

    lab3_first_react_app

---
+++ first_react_app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { ProductListItem } from './components/ProductListItem';
+
+function App() {
+
+      return (
+        <div className="App">
+          <ProductListItem id={1} name={"PC"} type={"Computers"} price={4000}></ProductListItem>
+        </div>
+      );
+}
+export default App;
+
---
+++ first_react_app/src/models/IProductListState.tsx
@@ -0,0 +1,7 @@
+import IProduct from "./IProduct";
+
+export interface IProductListState{
+
+    products: Array<IProduct>;
+    name: string;
+}
